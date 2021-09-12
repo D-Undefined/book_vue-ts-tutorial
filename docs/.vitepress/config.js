4cc36e328a7342c36f4ea47319153c784426ed76
kazuki-komori <komokazu.nankai@gmail.com>
2021-09-12 22:36:21 +0900

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'TypeScript で書く Vue.js 入門',
+  description: 'composition API 入門',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'はじめに',
+        link: '/guide/0-はじめに',
+      },
+      {
+        text: 'Composition API とは',
+        link: '/guide/1-what-is-composition-api',
+      },
+      {
+        text: 'ハンズオン',
+        link: '/guide/2-ハンズオン',
+      }
+    ],
+    repoLabel: 'Github',
+    editLinks: true,
+    editLinkText: 'ページに不明点や誤字等があれば、Github にて修正を提案してください！',
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroText: TypeScript で書く Vue.js 入門
+actionText: はじめる
+actionLink: /guide/0-はじめに
+footer: MIT Licensed | Copyright © 2021 kazuki komori
+---
+
